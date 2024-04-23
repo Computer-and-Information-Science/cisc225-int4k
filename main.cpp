3f829f64e4eef87ae3098ce08f518c81fa7cf315
Norm Lippincott <nl@acm.org>
2024-04-23 13:28:44 -0400

    Added example program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
+// Computes N! using int4k class
+#include "int4k.h"
 #include <iostream>
 using namespace std;
 
-int main () {
-    cout << "Hello World!\n";
+int main (int argc, char* argv[]) {
+
+	// Get value of N from command line, or prompt for N if not provided
+	int4k n;
+	if (argc > 1) {
+		n = argv[1];
+		cout << "Computing " << n << "!...\n";
+	}
+	else {
+		cout << "Computes n!\n";
+		cout << "Enter n: ";
+		cin >> n;
+	}
+
+	// Compute and output n!
+	int4k factorial("1");
+	for (int4k i = 1; i <= n; i++)
+		factorial *= i;
+	cout << factorial << endl;
 }
